69e250f59334925280711319cb36c795253673bb
Nooaah <noah.chtl@gmail.com>
2020-04-19 22:25:12 +0200

    To Buffer

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 var PORT = 8889;
-var HOST = '192.168.10.1';
+var HOST = '127.0.0.1';
 
 var dgram = require('dgram');
-var message = new Buffer('command');
-
 var client = dgram.createSocket('udp4');
 
-client.send(message, 0, PORT, HOST, function (err, bytes) {
-    if (err) throw err;
-    console.log('UDP client message sent to ' + HOST + ':' + PORT);
-});
 
-client.send('takeoff', 0, PORT, HOST, function (err, bytes) {
-    if (err) throw err;
-    console.log('UDP client message sent to ' + HOST + ':' + PORT);
-});
-
+console.log('[✓] Current connection to DJI Tello...');
 setTimeout(() => {
-    client.send('land', 0, PORT, HOST, function (err, bytes) {
-        if (err) throw err;
-        console.log('UDP client message sent to ' + HOST + ':' + PORT);
-    });
-}, 5000);
+    console.log('[✓] Connected to Tello 🚁');
+    setTimeout(() => {
+        launch();
+    }, 500);
+}, 500);
 
-client.connect(PORT, HOST, function () {
-    console.log('Vous êtes connecté');
-});
+function launch() {
+    var message = new Buffer('command');
+    client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('[✓] You are taking control of Tello 🎮');
+    });
+
+    var message = new Buffer('takeoff');
+    client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('[✓] You Tello Take Off 🛫');
+    });
+
+    setTimeout(() => {
+        var message = new Buffer('land');
+        client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+            if (err) throw err;
+            console.log('[✓] You Tello Land 🛬');
+        });
+    }, 5000);
+}
