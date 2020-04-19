10144940659ed3c842e3483b9b08fd3c97b9c8a2
Nooaah <noah.chtl@gmail.com>
2020-04-19 22:02:26 +0200

    Make UDP request

---
+++ index.js
@@ -0,0 +1,28 @@
+var PORT = 8889;
+var HOST = '192.168.10.1';
+
+var dgram = require('dgram');
+var message = new Buffer('command');
+
+var client = dgram.createSocket('udp4');
+
+client.send(message, 0, PORT, HOST, function (err, bytes) {
+    if (err) throw err;
+    console.log('UDP client message sent to ' + HOST + ':' + PORT);
+});
+
+client.send('takeoff', 0, PORT, HOST, function (err, bytes) {
+    if (err) throw err;
+    console.log('UDP client message sent to ' + HOST + ':' + PORT);
+});
+
+setTimeout(() => {
+    client.send('land', 0, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP client message sent to ' + HOST + ':' + PORT);
+    });
+}, 5000);
+
+client.connect(PORT, HOST, function () {
+    console.log('Vous êtes connecté');
+});
